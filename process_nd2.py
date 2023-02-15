92f79c4b203996bc4eaab5785a2ae455674dc5f9
snkeegan <sarah@fenyolab.org>
2023-02-15 14:29:47 -0500

    Updates

--- process_nd2.py
+++ process_nd2.py
@@ -3,10 +3,8 @@ import nd2
 import glob
 from tifffile import imread, imwrite
 
-base_dir = "/Users/snk218/Dropbox (NYU Langone Health)/mac_files/holtlab/data_and_results/Farida_LINE1"
+base_dir = "/Users/snk218/Dropbox (NYU Langone Health)/mac_files/holtlab/data_and_results/LINE1/RNA-ORF1 Colocalization"
 folder="20221030_LINE1"
-
-param_file=f"{base_dir}/spot_counting_parameters"
 
 for file in glob.glob(f"{base_dir}/{folder}/*.nd2"):
     file_root=os.path.splitext(os.path.split(file)[1])[0]
